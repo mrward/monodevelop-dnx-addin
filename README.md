7f91e5c2a572efc04876da173634910956082624
Matt Ward <ward.matt@gmail.com>
2015-07-11 12:27:23 +0100

    Create empty DNX addin.

---
+++ README.md
@@ -0,0 +1,3 @@
+# DNX Support for MonoDevelop and Xamarin Studio
+
+Provides [DNX and ASP.NET 5](http://docs.asp.net/en/latest/dnx/index.html) support for MonoDevelop and Xamarin Studio.
---
+++ src/MonoDevelop.Dnx/Properties/AddinInfo.cs
@@ -0,0 +1,12 @@
+﻿using Mono.Addins;
+
+[assembly:Addin ("Dnx",
+	Namespace = "MonoDevelop",
+	Version = "0.1",
+	Category = "IDE extensions")]
+
+[assembly:AddinName ("DNX")]
+[assembly:AddinDescription ("Adds DNX and ASP.NET 5 support.")]
+
+[assembly:AddinDependency ("Core", "5.0")]
+[assembly:AddinDependency ("Ide", "5.0")]
