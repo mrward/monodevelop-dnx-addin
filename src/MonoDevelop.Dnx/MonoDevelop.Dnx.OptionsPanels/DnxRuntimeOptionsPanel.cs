389c9b467d17fdfe1ed0bdc177945a2cdecd72a4
Matt Ward <ward.matt@gmail.com>
2015-10-03 18:46:53 +0100

    DNX runtime version can be changed in project options.
    Added a project options panel: Build - DNX Runtime which allows the
    DNX SDK version number to be changed in the global.json file.
    
    This is a project options panel and not a solution options panel so
    option can be hidden in the dialog if the project is not a DNX
    project. Custom conditions to show/hide options for a solution are
    not possible. This would mean the DNX runtime options page could not
    be hidden if a solution did not contain any DNX projects.
    
    The list of DNX runtime versions is currently hard coded. The
    installed versions should be determined and listed instead.
    
    The format of the global.json file is currently fixed to use tabs.
    It should use a custom code formatter to allow the formatting to be
    customised.

---
+++ src/MonoDevelop.Dnx/MonoDevelop.Dnx.OptionsPanels/DnxRuntimeOptionsPanel.cs
@@ -0,0 +1,112 @@
+﻿//
+// DnxRuntimeOptionsPanel.cs
+//
+// Author:
+//       Matt Ward <ward.matt@gmail.com>
+//
+// Copyright (c) 2015 Matthew Ward
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+using System.Collections.Generic;
+using System.Linq;
+using MonoDevelop.Core;
+using MonoDevelop.Ide.Gui.Dialogs;
+
+namespace MonoDevelop.Dnx.OptionsPanels
+{
+	public class DnxRuntimeOptionsPanel : OptionsPanel
+	{
+		Gtk.ComboBox dnxRuntimeVersionComboBox;
+		GlobalJsonFile globalJsonFile;
+		List<string> dnxRuntimeVersions;
+		string originalDnxRuntimeVersion;
+
+		public override Gtk.Widget CreatePanelWidget ()
+		{
+			var vbox = new Gtk.VBox ();
+			var hbox = new Gtk.HBox ();
+			var label = new Gtk.Label (GettextCatalog.GetString ("DNX Runtime Version:"));
+			label.Xalign = 0;
+			hbox.PackStart (label, false, false, 5);
+
+			dnxRuntimeVersionComboBox = Gtk.ComboBox.NewText ();
+			hbox.PackStart (dnxRuntimeVersionComboBox, true, true, 5);
+
+			PopulateOptionsPanel ();
+
+			vbox.PackStart (hbox);
+			vbox.ShowAll ();
+			return vbox;
+		}
+
+		void PopulateOptionsPanel ()
+		{
+			dnxRuntimeVersions = GetDnxRuntimeVersions ().ToList ();
+			globalJsonFile = GlobalJsonFile.Read ((DnxProject)DataObject);
+			if (globalJsonFile.Exists) {
+				originalDnxRuntimeVersion = globalJsonFile.DnxRuntimeVersion;
+			}
+
+			if (globalJsonFile == null)
+				return;
+
+			if (globalJsonFile.Exists) {
+				AddDnxRuntimeVersionsToComboBox ();
+				SelectDnxRuntimeVersionInComboBox (globalJsonFile.DnxRuntimeVersion);
+			}
+		}
+
+		IEnumerable<string> GetDnxRuntimeVersions ()
+		{
+			return new string[] {
+				"1.0.0-beta4",
+				"1.0.0-beta5",
+				"1.0.0-beta6",
+				"1.0.0-beta7",
+			};
+		}
+
+		void AddDnxRuntimeVersionsToComboBox ()
+		{
+			foreach (string version in dnxRuntimeVersions) {
+				dnxRuntimeVersionComboBox.AppendText (version);
+			}
+		}
+
+		public override void ApplyChanges ()
+		{
+			if (globalJsonFile == null)
+				return;
+
+			globalJsonFile.DnxRuntimeVersion = dnxRuntimeVersionComboBox.ActiveText;
+			if (originalDnxRuntimeVersion != globalJsonFile.DnxRuntimeVersion) {
+				globalJsonFile.Save ();
+				FileService.NotifyFileChanged (globalJsonFile.Path);
+			}
+		}
+
+		void SelectDnxRuntimeVersionInComboBox (string version)
+		{
+			dnxRuntimeVersionComboBox.Active = dnxRuntimeVersions.IndexOf (version);
+		}
+	}
+}
+
---
+++ src/MonoDevelop.Dnx/MonoDevelop.Dnx/GlobalJsonFile.cs
@@ -0,0 +1,119 @@
+﻿//
+// GlobalJsonFile.cs
+//
+// Author:
+//       Matt Ward <ward.matt@gmail.com>
+//
+// Copyright (c) 2015 Matthew Ward
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+using System.IO;
+using System.Text;
+using MonoDevelop.Core;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MonoDevelop.Dnx
+{
+	public class GlobalJsonFile
+	{
+		FilePath filePath;
+		Encoding encoding = Encoding.UTF8;
+		JObject jsonObject;
+		JObject sdkObject;
+
+		GlobalJsonFile (FilePath filePath)
+		{
+			this.filePath = filePath;
+		}
+
+		public static GlobalJsonFile Read (DnxProject project)
+		{
+			var jsonFile = new GlobalJsonFile (project.ParentSolution.BaseDirectory.Combine ("global.json"));
+			jsonFile.Read ();
+			return jsonFile;
+		}
+
+		public bool Exists { get; private set; }
+
+		public string DnxRuntimeVersion { get; set; }
+
+		void Read ()
+		{
+			Exists = JsonFileExists ();
+			if (!Exists)
+				return;
+
+			using (var fileStream = File.OpenRead (filePath)) {
+				using (var reader = new StreamReader (fileStream)) {
+					using (var jsonReader = new JsonTextReader (reader)) {
+						jsonObject = JObject.Load (jsonReader);
+						if (reader.CurrentEncoding != null) {
+							encoding = reader.CurrentEncoding;
+						}
+					}
+				}
+			}
+
+			ReadPropertiesFromJsonObject ();
+		}
+
+		bool JsonFileExists ()
+		{
+			return File.Exists (filePath);
+		}
+
+		void ReadPropertiesFromJsonObject ()
+		{
+			JToken sdkToken;
+			if (!jsonObject.TryGetValue ("sdk", out sdkToken))
+				return;
+
+			sdkObject = sdkToken as JObject;
+			if (sdkObject == null)
+				return;
+
+			JToken version;
+			if (!sdkObject.TryGetValue ("version", out version))
+				return;
+
+			DnxRuntimeVersion = version.ToString ();
+		}
+
+		public void Save ()
+		{
+			sdkObject["version"] = new JValue (DnxRuntimeVersion);
+			using (var writer = new StreamWriter (filePath, false, encoding)) {
+				using (var jsonWriter = new JsonTextWriter (writer)) {
+					jsonWriter.Formatting = Formatting.Indented;
+					jsonWriter.Indentation = 1;
+					jsonWriter.IndentChar = '\t';
+					jsonObject.WriteTo (jsonWriter);
+				}
+			}
+		}
+
+		public FilePath Path {
+			get { return filePath; }
+		}
+	}
+}
+
