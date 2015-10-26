5311d955e498bc84aba39eb197925ef9a2d74ad7
Matt Ward <ward.matt@gmail.com>
2015-10-26 21:23:31 +0000

    Update project templates for beta 8.
    Still missing AssemblyInfo.cs, NuGet.Config and web.config file.

--- src/MonoDevelop.Dnx/Templates/Projects/EmptyWeb/EmptyWebProject.Startup.cs
+++ src/MonoDevelop.Dnx/Templates/Projects/EmptyWeb/EmptyWebProject.Startup.cs
@@ -15,6 +15,8 @@ namespace ${Namespace}
 
 		public void Configure(IApplicationBuilder app)
 		{
+			app.UseIISPlatformHandler();
+
 			app.Run(async (context) => {
 				await context.Response.WriteAsync("Hello World!");
 			});
