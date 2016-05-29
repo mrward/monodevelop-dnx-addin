e2ccedf84ada4f14c42388b95a323845c7fe4ec9
Matt Ward <ward.matt@gmail.com>
2016-05-29 15:41:37 +0100

    Debugging now working for full .NET framework projects.
    It is now possible to debug .NET Core projects that target the
    full .NET Framework. It is still not possible to debug .NET Core
    projects that target .NET Core.

--- src/MonoDevelop.Dnx/MonoDevelop.Dnx/DotNetCoreDebuggerEngine.cs
+++ src/MonoDevelop.Dnx/MonoDevelop.Dnx/DotNetCoreDebuggerEngine.cs
@@ -6,16 +6,16 @@ using MonoDevelop.Debugger.Soft;
 
 namespace MonoDevelop.Dnx
 {
-	public class DnxDebuggerEngine : SoftDebuggerEngine
+	public class DotNetCoreDebuggerEngine : SoftDebuggerEngine
 	{
 		public override bool CanDebugCommand (ExecutionCommand cmd)
 		{
-			return cmd is DnxProjectExecutionCommand && !Platform.IsWindows;
+			return cmd is DotNetCoreExecutionCommand && !Platform.IsWindows;
 		}
 
 		public override DebuggerStartInfo CreateDebuggerStartInfo (ExecutionCommand c)
 		{
-			var dotNetCommand = DnxExecutionHandler.ConvertCommand ((DnxProjectExecutionCommand)c);
+			var dotNetCommand = DotNetCoreExecutionHandler.ConvertCommand ((DotNetCoreExecutionCommand)c);
 			return base.CreateDebuggerStartInfo (dotNetCommand);
 		}
 	}
