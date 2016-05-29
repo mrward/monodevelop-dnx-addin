bacb44a60c313895bcd74ebd880f9878d88d3c6a
Matt Ward <ward.matt@gmail.com>
2016-05-29 15:36:54 +0100

    Try other architecture for output file when not on Windows.
    When using Mono it is possible to debug a x64 executable even though
    the current IDE process is x86. So when looking for the output
    executable the addin now tries both the current IDE architecture and
    then the alternative architecture if no file is found.
    
    This basically reverts the commit below but adds the restriction to
    only check other architectures when not on Windows:
    
    55a88e83416de080c14467f48e2fa6cee3eb12d0

--- src/MonoDevelop.DotNet.ProjectModel/MonoDevelop.DotNet.ProjectModel/DotNetProjectBuildOutputAssemblyResolver.cs
+++ src/MonoDevelop.DotNet.ProjectModel/MonoDevelop.DotNet.ProjectModel/DotNetProjectBuildOutputAssemblyResolver.cs
@@ -25,6 +25,7 @@
 // THE SOFTWARE.
 //
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -53,7 +54,14 @@ namespace MonoDevelop.DotNet.ProjectModel
 
 			var runtimeIds = RuntimeEnvironmentRidExtensions.GetAllCandidateRuntimeIdentifiers ().ToList ();
 
-			return ResolveOutputPath (frameworkContexts, workspace, configuration, runtimeIds);
+			string outputPath = ResolveOutputPath (frameworkContexts, workspace, configuration, runtimeIds);
+			if (outputPath == null && RuntimeEnvironment.OperatingSystemPlatform != Platform.Windows) {
+				// Try alternative architecture.
+				runtimeIds = runtimeIds.Select (runtimeId => ConvertArchitecture (runtimeId)).ToList ();
+				return ResolveOutputPath (frameworkContexts, workspace, configuration, runtimeIds);
+			}
+
+			return outputPath;
 		}
 
 		string ResolveOutputPath (
@@ -72,6 +80,23 @@ namespace MonoDevelop.DotNet.ProjectModel
 			}
 
 			return null;
+		}
+
+		static string ConvertArchitecture (string runtimeId)
+		{
+			string currentArchitecture = RuntimeEnvironment.RuntimeArchitecture;
+			string otherArchitecture = GetAlternativeArch ();
+			return runtimeId.Replace (currentArchitecture, otherArchitecture);
+		}
+
+		/// <summary>
+		/// Returns the alternative architecture to the current architecture.
+		/// So if the current architecture is x86 then this method returns x64 and
+		/// vice versa.
+		/// </summary>
+		static string GetAlternativeArch ()
+		{
+			return Environment.Is64BitProcess ? "x86" : "x64";
 		}
 	}
 }
