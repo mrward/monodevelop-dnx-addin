e1dc9f578406edd4847c8d04a1188d54c68c3e3d
Matt Ward <ward.matt@gmail.com>
2016-06-28 19:51:16 +0100

    Update project templates for .NET Core 1.0 (preview 2)

--- src/MonoDevelop.Dnx/Templates/Projects/EmptyKestrelWeb/EmptyKestrelWebProject.Startup.cs
+++ src/MonoDevelop.Dnx/Templates/Projects/EmptyKestrelWeb/EmptyKestrelWebProject.Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace ${Namespace}
 {
@@ -14,8 +15,14 @@ namespace ${Namespace}
 		{
 		}
 
-		public void Configure(IApplicationBuilder app)
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
 		{
+			loggerFactory.AddConsole();
+
+			if (env.IsDevelopment()) {
+				app.UseDeveloperExceptionPage();
+			}
+
 			app.Run(async (context) => {
 				await context.Response.WriteAsync("Hello World!");
 			});
